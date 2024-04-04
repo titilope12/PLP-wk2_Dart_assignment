03759a83b54987ab00a6e6484c0ab63db67573ff
titilope12 <133906978+titilope12@users.noreply.github.com>
2024-04-04 09:35:18 +0100

    Add files via upload
    First commit on week two assignment on datatype on dart

---
+++ index.dart
@@ -0,0 +1,26 @@
+void main() {
+  int val = 35; //int takes integer numbers
+  double num1 = 12.55; // double takes floating point values
+  num val1 = 25; // num takes both floatun point and integer values
+  String abc = "Victor"; //String store text
+  List<int> value = [
+    1,
+    3,
+    5,
+    6
+  ]; // List is a collection of object the same datatype
+  bool isReady =
+      true; //bool datatype (boolean) is one that accept true or false value
+  Map<String, int> grades = {
+    "victor": 60,
+    "yemi": 56,
+    "ediomo": 80,
+  }; //Map is adynamic collection that represents aset of value as key-value pairs
+  print(val);
+  print(num1);
+  print(val1);
+  print(abc);
+  print(value);
+  print(isReady);
+  print(grades);
+}
